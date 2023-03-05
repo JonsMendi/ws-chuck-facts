939777c011ae76bc33318287aa53c798ca3d1529
Joao Mendonca <jom489@node-robotics.com>
2023-03-05 12:27:10 +0100

    main: Clean up code

--- pages/chuck-body.tsx
+++ pages/chuck-body.tsx
@@ -18,11 +18,12 @@ export default function ChuckBody() {
       <div className="flex items-center">
         <div className="relative ml-20 py-10">
           <Canvas className="cursor-pointer" onClick={fetchFact}>
+            <Suspense fallback={null}>
               <OrbitControls enableZoom={false} />
               <ambientLight intensity={0.5} />
               <directionalLight position={[-2, 5, 2]} intensity={1} />
-              <Suspense fallback={null} />
               <ChuckBrain />
+            </Suspense>
           </Canvas>
         </div>
         <div className="text-center mx-auto pt-20 max-w-[800px] min-w-[800px]">
